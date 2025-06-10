6948aedc55f9f9c49b6e3afbab05fa3a5ce3b29e
Mark Seah <seah.marksc@gmail.com>
2025-06-10 10:31:40 +0800

    Initial Commit

---
+++ checkoutplayground-ios/checkoutplayground_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkoutplayground_iosApp.swift
+//  checkoutplayground-ios
+//
+//  Created by Mark Seah on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct checkoutplayground_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
